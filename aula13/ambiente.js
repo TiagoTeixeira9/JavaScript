072f0e5a5c4dce4621de53989175b272fcc41cec
Tiago Teixeira <108500810+TiagoTeixeira9@users.noreply.github.com>
2023-09-08 19:13:56 +0900

    aula 13

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c = 1
+do {
+    console.log("Tudo bem!")
+    c++
+} while (c <= 500)
+
+
+/* 
+var c = 1
+while (c <= 10) {
+    console.log("Tudo bem!")
+    c++ // c = c + 1
+}
+*/
+
+
+/*
+console.log("Tudo bem!")
+console.log("Tudo bem!")
+console.log("Tudo bem!")
+console.log("Tudo bem!")
+console.log("Tudo bem!")
+console.log("Tudo bem!")
+*/
